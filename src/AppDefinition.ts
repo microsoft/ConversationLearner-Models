75c132fe55f7112075ef7c2b373ba6e322db55cf
Lars Liden <larsliden@gmail.com>
2018-01-23 15:36:00 -0800

    Alter AppDef to support downloading of source

--- src/AppDefinition.ts
+++ src/AppDefinition.ts
@@ -1,10 +1,12 @@
 import { EntityBase } from './Entity';
 import { ActionBase } from './Action';
+import { TrainDialog } from './index';
 
 export class AppDefinition
 {
     public entities : EntityBase[];
     public actions : ActionBase[];
+    public trainDialogs : TrainDialog[];
 
     public constructor(init?:Partial<AppDefinition>)
     {
