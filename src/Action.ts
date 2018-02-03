f593115f9594dbff10781df1ee62204684b4e5f4
Matt Mazzola <mattm@microsoft.com>
2018-02-02 17:33:03 -0800

    Conslidate action paylod, and make ActionArgument values TextPayload or string

--- src/Action.ts
+++ src/Action.ts
@@ -1,52 +1,36 @@
-import { ScoredAction } from './Score';
-import { ActionPayload, ActionArgument } from './ActionPayload';
+import { ScoredAction } from './Score'
 
 export const ActionTypes =
-{
-    TEXT : "TEXT",
-    API_LOCAL : "API_LOCAL",
-    // API_AZURE : "API_AZURE", TODO
-    CARD : "CARD"
-}
-
-export class ActionMetaData
-{
-    // Action Type
-    public actionType : string;
-
-    public constructor(init?:Partial<ActionMetaData>)
     {
-        (<any>Object).assign(this, init);
+        TEXT: "TEXT",
+        API_LOCAL: "API_LOCAL",
+        // API_AZURE : "API_AZURE", TODO
+        CARD: "CARD"
     }
 
-    public Equal(metaData : ActionMetaData) : boolean
-    {
-        return (this.actionType == metaData.actionType);
-    }
+export interface ActionMetaData {
+    actionType: string
 }
 
-export class ActionBase
-{
-    public actionId : string;
-    public payload : string;
-    public isTerminal : boolean;
-    public requiredEntities : string[];
-    public negativeEntities : string[];
-    public suggestedEntity : string;
-    public version : number;
-    public packageCreationId : number;
-    public packageDeletionId : number;
-    public metadata : ActionMetaData;
+export class ActionBase {
+    public actionId: string;
+    public payload: string;
+    public isTerminal: boolean;
+    public requiredEntities: string[];
+    public negativeEntities: string[];
+    public suggestedEntity: string | null;
+    public version: number;
+    public packageCreationId: number;
+    public packageDeletionId: number;
+    public metadata: ActionMetaData;
 
     public constructor(init?:Partial<ActionBase>)
     {
-        this.metadata = new ActionMetaData();
         (<any>Object).assign(this, init);
     } 
 
     /** Return payload for an action */
-    public static GetPayload(action : ActionBase | ScoredAction) : string
-    {
+    public static GetPayload(action: ActionBase | ScoredAction): string {
         if (!action.metadata || action.metadata.actionType === ActionTypes.TEXT) {
             /**
              * For backwards compatibility check if payload is of new TextPayload type
@@ -71,37 +55,43 @@ export class ActionBase
     }
 
     /** Return arguments for an action */
-    public static GetActionArguments(action : ActionBase | ScoredAction) : ActionArgument[] | null
-    {
-        if (!action.metadata || action.metadata.actionType == ActionTypes.TEXT) {
-            return null;
+    public static GetActionArguments(action: ActionBase | ScoredAction): ActionArgument[] {
+        if (!action.metadata || action.metadata.actionType === ActionTypes.TEXT) {
+            return []
         }
         if (action.metadata.actionType !== ActionTypes.TEXT) {
-            let actionPayload = JSON.parse(action.payload) as ActionPayload;
-            return actionPayload.arguments;
+            let actionPayload = JSON.parse(action.payload) as ActionPayload
+            return actionPayload.arguments
         }
-        return null;
+        return []
+    }
+
+    public static GetActionArgumentValuesAsPlainText(action: ActionBase | ScoredAction): string[] {
+        if (!action.metadata || action.metadata.actionType === ActionTypes.TEXT) {
+            return []
+        }
+        if (action.metadata.actionType !== ActionTypes.TEXT) {
+            let actionPayload = JSON.parse(action.payload) as ActionPayload
+            return getActionArgumentValuesAsPlainText(actionPayload.arguments)
+        }
+        return []
     }
 }
 
-export class ActionList
-{
-  
-    public actions : ActionBase[];
+export class ActionList {
 
-    public constructor(init?:Partial<ActionList>)
-    {
+    public actions: ActionBase[];
+
+    public constructor(init?: Partial<ActionList>) {
         (<any>Object).assign(this, init);
     }
 }
 
-export class ActionIdList
-{
-  
-    public actionIds : string[];
+export class ActionIdList {
 
-    public constructor(init?:Partial<ActionIdList>)
-    {
+    public actionIds: string[];
+
+    public constructor(init?: Partial<ActionIdList>) {
         (<any>Object).assign(this, init);
     }
 }
@@ -110,3 +100,19 @@ export interface TextPayload {
     text: string
     json: any
 }
+
+export interface ActionPayload {
+    payload: string
+    arguments: ActionArgument[]
+}
+
+export interface ActionArgument {
+    parameter: string
+    value: TextPayload | string
+}
+
+export const getActionArgumentValuesAsPlainText = (actionArguments: ActionArgument[]): string[] =>
+    actionArguments
+        .map(aa => typeof aa.value === 'string'
+            ? aa.value
+            : aa.value.text)
