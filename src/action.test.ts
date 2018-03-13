54cb4623561cb6319868d324ac9c7af2eb610b9a
Matt Mazzola <mattm@microsoft.com>
2018-03-13 12:01:22 -0700

    Fix tests

--- src/action.test.ts
+++ src/action.test.ts
@@ -1,4 +1,4 @@
-import { IActionArgument, ActionTypes, ActionBase, ActionPayload, TextPayload } from './Action'
+import { ActionArgument, ActionTypes, ActionBase, ActionPayload, TextPayload, IActionArgument } from './Action'
 
 const createEmptyAction = (): ActionBase => ({
   actionId: '',
@@ -259,6 +259,8 @@ const cardActionArguments: IActionArgument[] = [
     }
   }
 ]
+
+const expectedCardActionArguments = cardActionArguments.map(aa => new ActionArgument(aa))
 const cardAction: ActionBase = {
   ...createEmptyAction(),
   actionType: ActionTypes.CARD,
@@ -343,7 +345,7 @@ describe('Action', () => {
       const actionArguments = ActionBase.GetActionArguments(cardAction)
 
       // Assert
-      expect(actionArguments).toEqual(cardActionArguments)
+      expect(actionArguments).toEqual(expectedCardActionArguments)
     })
   })
 })
