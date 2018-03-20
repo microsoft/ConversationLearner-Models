5d3ecd42cf6344105354cdc53faa5504782a3ff6
Lars Liden <larsliden@gmail.com>
2018-03-20 15:42:26 -0700

    Added ReplayErrors

---
+++ src/ReplayError.ts
@@ -0,0 +1,34 @@
+export enum ReplayErrorType {
+  MissingAction = 'MissingAcion',
+  MissingEntity = 'MissingEntity',
+  ActionUnavailable = 'ActionUnavailable',
+  EntityDiscrepancy = 'EntityDiscrepancy'
+}
+
+export class ReplayError {
+  constructor(public type: ReplayErrorType) {}
+}
+
+export class ReplayErrorMissingAction extends ReplayError {
+  constructor(public lastUserInput: string) {
+    super(ReplayErrorType.MissingAction)
+  }
+}
+
+export class ReplayErrorMissingEntity extends ReplayError {
+  constructor(public value: string) {
+    super(ReplayErrorType.MissingEntity)
+  }
+}
+
+export class ReplayErrorActionUnavailable extends ReplayError {
+  constructor(public lastUserInput: string) {
+    super(ReplayErrorType.ActionUnavailable)
+  }
+}
+
+export class ReplayErrorEntityDiscrepancy extends ReplayError {
+  constructor(public lastUserInput: string, public originalEntities: string[], public newEntities: string[]) {
+    super(ReplayErrorType.EntityDiscrepancy)
+  }
+}
