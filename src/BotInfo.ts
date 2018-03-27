7ae4e028247d8dc27af2543ca370bfba13371cc3
Matt Mazzola <mattm@microsoft.com>
2018-03-27 12:16:44 -0700

    Simulate user from SDK based on luis authoring key as user id (#71)

--- src/BotInfo.ts
+++ src/BotInfo.ts
@@ -1,8 +1,14 @@
 import { Template } from './Template'
 import { CallbackAPI } from './CallbackAPI'
 
+export interface IUser {
+  name: string
+  id: string
+}
+
 /** Information about the running bot */
 export interface BotInfo {
+  user: IUser
   /** APICallbacks available to BLIS */
   callbacks: CallbackAPI[]
   templates: Template[]
