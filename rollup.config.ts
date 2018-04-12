2f19dd2ed82c08426a68fff79d2540984104c740
Lars Liden <larsliden@gmail.com>
2018-04-12 16:39:39 -0700

    more case fixes

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,7 +6,7 @@ import typescript from 'rollup-plugin-typescript2'
 
 const pkg = require('./package.json')
 
-const libraryName = 'ConversationLearner-Models'
+const libraryName = 'conversationLearner-models'
 
 export default {
   input: `src/${libraryName}.ts`,
--- src/filledEntity.test.ts
+++ src/filledEntity.test.ts
@@ -1,4 +1,4 @@
-import { FilledEntity, FilledEntityMap, filledEntityValueAsString, MemoryValue } from './ConversationLearner-Models'
+import { FilledEntity, FilledEntityMap, filledEntityValueAsString, MemoryValue } from './conversationLearner-models'
 
 const createFilledEntity = (values: string[]): FilledEntity => {
   return {
