9385882336a037aa26ec65cda11a0b2ee7b7dd68
Thomas Park <thpar@microsoft.com>
2019-08-15 08:59:13 -0700

    feat: new utils files

---
+++ src/OBIUtils.test.ts
@@ -0,0 +1,123 @@
+import { LGItem, OBIDialogParser } from "./OBIUtils";
+
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// let objectToMap = (obj: Object) => {
+//     let map = new Map()
+//     for (let key in obj) {
+//         let val = String(obj[key])
+//         map.set(key, val)
+//     }
+//     return map
+// }
+
+interface TestData {
+    tag: string
+    text: string
+    suggestions: string[]
+}
+
+const testDataToMap = (data : TestData[]) => {
+    let output = new Map<string, LGItem>()
+    for (const datum of data) {
+        let d = {
+            text: datum.text,
+            suggestions: datum.suggestions
+        }
+        output.set(datum.tag, d)
+    }
+    return output
+}
+
+describe('OBIutils', () => {
+    describe('OBIDialogParser', () => {
+        test('Given a well-formed input, verifies the expected output', () => {
+            let input = `
+            # option0
+            - \`\`\`
+            Hi! I'm a virtual agent. I can help with account questions, orders, store information, and more.
+            [Suggestions=]
+            \`\`\`
+            # option1
+            - \`\`\`
+            If you'd like to speak to a human agent, let me know at any time.
+            [Suggestions=Talk to agent|Goodbye]
+            \`\`\`
+            `
+            const map = OBIDialogParser.parseLGString(input)
+            let expected: Map<string, LGItem> = testDataToMap([
+                {
+                    tag: 'option0',
+                    text: `Hi! I'm a virtual agent. I can help with account questions, orders, store information, and more.`,
+                    suggestions: []
+                },
+                {
+                    tag: 'option1',
+                    text: `If you'd like to speak to a human agent, let me know at any time.`,
+                    suggestions: ['Talk to agent', 'Goodbye']
+
+                }
+            ])
+            expect(map).toMatchObject(expected)
+        })
+        test('Validate exception on missing expected tokens', () => {
+            let inputs = [
+                {
+                    missing_str: '-',
+                    input: `
+                # option0
+                \`\`\`
+                This config is missing a hyphen
+                [Suggestions=]
+                \`\`\`
+                `},
+                {
+                    // Not '```' since the input contains it later.
+                    missing_str: '```',
+                    input: `
+                # option0
+                -
+                This config is missing the leading 3 backticks
+                [Suggestions=]
+                \`\`\`
+                `},
+                {
+                    missing_str: '[',
+                    input: `
+                # option0
+                - \`\`\`
+                This config is missing an open bracket
+                Suggestions=]
+                \`\`\`
+                `},
+                {
+                    missing_str: ']',
+                    input: `
+                # option0
+                - \`\`\`
+                This config is missing a close bracket
+                [Suggestions=
+                \`\`\`
+                `},
+                // Don't test for trailing backticks, these are not currently required.
+            ]
+            for (const data of inputs) {
+                try {
+                    console.log(`Testing ${data.input}`)
+                    OBIDialogParser.parseLGString(data.input)
+                    fail('Did not get expected exception')
+                } catch (e) {
+                    if (e instanceof RangeError) {
+                        expect(e.message).toBe(`${data.missing_str} not found`)
+                    } else {
+                        fail('Unexpected error')
+                    }
+                }
+            }
+
+        })
+    })
+})
---
+++ src/OBIUtils.ts
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export interface LGItem {
+    text: string,
+    suggestions: string[]
+}
+
+export interface ComposerDialog {
+    //    dialogs: OBIDialog[]
+    luMap: Map<string, string[]>
+    lgMap: Map<string, LGItem>
+}
+
+const findOrThrow = (haystack_fn: (s: string) => number, needle: string): number => {
+    const i = haystack_fn(needle)
+    if (i === -1) throw new RangeError(`${needle} not found`)
+    return i
+}
+
+/**
+ * Finds the index of the first instance of needle in haystack, or throws an exception if not found.
+ */
+const findFirstOrThrow = (haystack: string, needle: string): number => {
+    return findOrThrow(haystack.indexOf.bind(haystack), needle)
+}
+
+/**
+ * Finds the index of the last instance of needle in haystack, or throws an exception if not found.
+ */
+const findLastOrThrow = (haystack: string, needle: string): number => {
+    return findOrThrow(haystack.lastIndexOf.bind(haystack), needle)
+}
+
+/**
+ * Parses OBI .dialog files from custom format into structured data.
+ */
+export class OBIDialogParser {
+
+    public static parseLGString(text: string): Map<string, LGItem> {
+        let output = new Map<string, LGItem>()
+        const items = text.split('# ')
+        for (let item of items) {
+            item = item.trim()
+            if (item.length === 0) continue
+            const key = item.substring(0, findFirstOrThrow(item, "-")).trim()
+            const backticksStart = findFirstOrThrow(item, "```")
+            const suggestionsStart = findLastOrThrow(item, "[")
+            if (backticksStart > suggestionsStart) throw new RangeError('``` not found')
+            const body = item.substring(backticksStart + 3, suggestionsStart).trim()
+            const suggestionTag = "[Suggestions="
+            const suggestionList = item.substring(findLastOrThrow(item, suggestionTag) + suggestionTag.length,
+                findLastOrThrow(item, "]"))
+            const suggestions = suggestionList.length > 0 ? suggestionList.split('|') : []
+            const element = {
+                text: body,
+                suggestions
+            }
+            output.set(key, element)
+        }
+        return output
+    }
+}
